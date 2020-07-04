d6a5c44211dfb3b4168028d95da0ccd5761afb45
Barbara <barbara04mir@gmail.com>
2020-07-04 20:42:53 +0300

    Comments for exceptions.py added

--- exceptions.py
+++ exceptions.py
@@ -1,14 +1,21 @@
 class DataBaseException(Exception):
+    # Exception that is raised if some problems happen during loading data from a base. Base class for more specific
+    # exceptions connected with databases
     pass
 
 
 class UserNotFound(DataBaseException):
+    # A child class inherited from DataBaseException. R if during searching for a user by specific parameters
+    # the user wasn't found.
     message = 'User is not found'
 
 
 class RegistrationException(Exception):
+    # A father-class for all exceptions raised during registration process
     pass
 
 
 class AlreadyRegistered(RegistrationException):
+    # A child class of RegistrationException, that is raised if someone has already registered his media-account for
+    # a certain record in the system-database.
     pass
