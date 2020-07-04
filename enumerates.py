39675745379e6adbba1931c091aecd3c35d64984
Barbara <barbara04mir@gmail.com>
2020-07-04 20:23:38 +0300

    Comments for bot.py, enumerates.py and user.py added The file message_handlers is deleted, because it was useless

--- enumerates.py
+++ enumerates.py
@@ -2,10 +2,13 @@ import enum
 
 
 class Media(enum.Enum):
+    """Enumerates all media and platforms this bot works with"""
     TELEGRAM = 1
 
 
 class TextLabels(enum.Enum):
+    """Enumerates all the Texts that can be sent to a user by the bot. They are used for specifying pieces of text
+    that must be loaded from a file and sent to a user."""
     MAIN_MENU = 1
     MAIN_MENU_GREETING = 2
     MAIN_MENU_GREETING_NEW = 3
@@ -15,13 +18,19 @@ class TextLabels(enum.Enum):
 
 
 class States(enum.Enum):
+    """This enumerator lists all the states the user can has. These states are a kind of spots on the tree of a dialog,
+    for example main_menu or choosing_task_draft"""
     MAIN_MENU = 1
 
 
 class MessageMarks(enum.Enum):
+    """This is a list of all markers that can be applied to a message for giving an external program some special
+    information about it."""
     UNREGISTERED = 1
 
 
 class KeyboardTypes(enum.Enum):
+    """Here are two types of a keyboard. They are taken from Telegram api and the info about it can be found in
+    python-telegram-bot library docs"""
     INLINE = 1
     REPLY = 2
--- user.py
+++ user.py
@@ -4,11 +4,20 @@ from exceptions import AlreadyRegistered
 
 
 class User:
-    def __init__(self, media=None, user_id=None, recording=None):
-        if recording:
-            user_elem = recording
+    """
+    Класс представляет собой объект пользователя. В экземпляре класса хранятся все данные о пользователе, которые
+    содержатся в базе данных или данные, которые еше не занесены в базу, но будут помещены туда (формат данных должен
+    совпадать с форматом airtable)
+    """
+    def __init__(self, media=None, user_id=None, record=None):
+        if record:
+            # If there is a record given, we use it
+            user_elem = record
         else:
+            # Otherwise load the data from the base
             user_elem = dataBase.get_user_by_id_in_media(media, user_id)
+
+        # all the fields from airtable-base turn into class object's fields
         self.__dict__ = user_elem['fields']
 
         # Deletion of needless (or inconvenient) fields
@@ -19,7 +28,7 @@ class User:
             if i.name.lower() + '_state' in self.__dict__.keys():
                 del self.__dict__[i.name.lower() + '_state']
 
-        # Adding some other important fields
+        # Adding some other important fields, which were not mentioned in database
         self.user_elem = user_elem
         self.base_id = user_elem['id']
         self.media_id = {}
@@ -33,6 +42,7 @@ class User:
         print(self.__dict__)
 
     def transform_into_record(self):
+        # Creates an airtable record from an object and returns it
         record = {'id': self.base_id}
         fields = self.__dict__.copy()
         keys = list(fields.keys())
@@ -53,15 +63,26 @@ class User:
         return record
 
     def update(self):
+        # updates the state of a user on a server
         record = self.transform_into_record()
         self.user_elem = dataBase.update_user(record)
 
     def set_state(self, media, state):
+        # changes the state parameter and loads the changes on a server
         self.state[media] = state.name
         self.update()
 
     @staticmethod
     def register(media, user_id, password):
+        """
+        This static method is used to register a new user using a unique password given to them by student development
+        department (or any other people that regulate the usage of this service). It loads all the information about the
+        user from the password and add his media-id to an airtable base.
+        :param media:
+        :param user_id:
+        :param password:
+        :return:
+        """
         user = User(dataBase.get_user_by_password(password))
         if user.media_id.get(media, None):
             raise AlreadyRegistered
